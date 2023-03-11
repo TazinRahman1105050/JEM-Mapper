77b0528b44059088b29b6af9e1c01b29155230e7
TazinRahman1105050 <tazinrahman158@gmail.com>
2023-03-11 15:13:02 -0800

    Update timers.h

--- includes/timers.h
+++ includes/timers.h
@@ -39,9 +39,7 @@
 #include <string.h>
 #include <assert.h>
 
-extern double k_gen_time, global_k_gen_time;
-extern double k_shift_time, global_k_shift_time;
-extern double kmer_count_time, global_kmer_count_time;
+
 extern double sel_time, global_sel_time;
 extern double alltoall_time, global_alltoall_time;
 extern double alltoallv_time, global_alltoallv_time;
@@ -62,23 +60,5 @@ extern double rd, global_rd_time;
 extern double ag_time, global_ag_time;
 extern double fl_time, global_fl_time;
 
-extern double contigs_time, global_contigs_time;
-extern double io_time, cleanup_time, vector_time;
-extern double count_time, global_count_time;
-extern double read_input_time, global_read_input_time;
-extern double pack_sbuf_time, global_pack_sbuf_time;
-extern double unpack_rbuf_time, global_unpack_rbuf_time;
-extern double unpack_rbuf_sort, global_unpack_rbuf_sort;
-extern double unpack_rbuf_insert, global_unpack_rbuf_insert;
-extern double unpack_rbuf_acc, global_unpack_rbuf_acc;
-extern double sl_win_time, global_sl_win_time;
-extern double sl_win_time_int, global_sl_win_time_int;
-extern double sl_lmer_freq, global_sl_lmer_freq;
 
-extern double vec_insert_time, global_vec_insert_time;
-extern double tmap_insert_time, global_tmap_insert_time;
-
-//P2 Timers
-extern double p2alltoall_time, p2global_alltoall_time;
-extern double p2alltoallv_time, p2global_alltoallv_time;
 
